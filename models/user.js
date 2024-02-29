868238167d6598c56f9f0624e2998a56f46305a8
shaikha <jshaikha0@gmail.com>
2024-02-29 12:50:13 +0300

    user model

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    passwordDigest: { type: String, required: true },
+    bio: { type: String },
+    artIds: [{ type: Schema.Types.ObjectId, ref: "Art" }],
+    collectionIds: [{ type: Schema.Types.ObjectId, ref: "Collection" }],
+  },
+  {
+    timestamps: true,
+  }
+)
+module.exports = mongoose.model("User", userSchema)
