b83f205432dda32cc38271496dc6572bdffe4023
9Jassim <jassim9.a@gmail.com>
2024-02-29 13:29:07 +0300

    create users route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,7 @@
 var express = require('express')
 var router = express.Router()
+const usersCtrl = require('../controllers/users')
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource')
-})
+router.get('/users/:id', usersCtrl.show)
 
 module.exports = router
