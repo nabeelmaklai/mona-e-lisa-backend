bfbf8c3ca948f1ce86dc432521c6a8f92ed4cb3f
9Jassim <jassim9.a@gmail.com>
2024-03-02 20:33:53 +0300

    fix collections route

--- routes/collections.js
+++ routes/collections.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-collectionsCtrl = require('../controllers/Collections')
+const collectionsCtrl = require('../controllers/Collections')
 
 router.post('/collections', collectionsCtrl.createCollection)
 router.get('/collections/:id', collectionsCtrl.showCollections)
