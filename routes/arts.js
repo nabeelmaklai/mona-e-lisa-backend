bc2358bd509bc49a6b21f571bf1a06c54ca9b7ff
Nabeel Maklai <nabilmaklai@hotmail.com>
2024-03-03 10:20:31 +0300

    added middleware to user only

--- routes/arts.js
+++ routes/arts.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const artsCtrl = require('../controllers/arts')
+const middleware = require('../middleware')
 
-router.post('/arts', artsCtrl.createArt)
+router.post(
+  '/arts',
+  middleware.stripToken,
+  middleware.verifyToken,
+  artsCtrl.createArt
+)
 router.post('/arts/:id/comments', artsCtrl.addComment)
 router.get('/arts/:id', artsCtrl.show)
 router.get('/arts', artsCtrl.index)
