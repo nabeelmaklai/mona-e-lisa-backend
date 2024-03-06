3e06a85916d9683726d1a762a44e0fb190276747
9Jassim <jassim9.a@gmail.com>
2024-03-06 09:07:06 +0300

    edit user controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,6 @@
 const User = require('../models/user')
 
 const show = async (req, res) => {
-  console.log('got to show in the controller')
   try {
     const user = await User.findById(req.params.id).populate([
       'artIds',
